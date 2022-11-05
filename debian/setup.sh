ed01fece414d14bbf5d4a7ee8828564af94a0aa2
Emmanuel Oh <emmaneugene@outlook.com>
2022-11-05 08:58:43 +0800

    Update brew formulae
    Add setup script for debian/ubuntu distros

--- debian/setup.sh
+++ debian/setup.sh
@@ -4,6 +4,8 @@ sudo apt-get update; sudo apt-get install zsh
 sudo chsh -s $(which zsh)
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Install plugins `zsh-autosuggestions` and `zsh-syntax-highlighting`
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
