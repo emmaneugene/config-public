916d506c283acd52050abc41fd8ee34c62f68bdd
Emmanuel Oh <emmaneugene@outlook.com>
2023-10-25 09:24:05 +0800

    Add `collections` import

--- dotfiles/pythonstartup.py
+++ dotfiles/pythonstartup.py
@@ -1,7 +1,7 @@
 # Startup script for interactive Python shells
 # To be symlinked to ~/.config/pythonstartup.py
 
-import csv, datetime as dt, json, math, os, sys
+import csv, datetime as dt, json, math, os, sys, collections
 
 try:
     import pandas as pd
