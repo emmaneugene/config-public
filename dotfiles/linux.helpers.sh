02a522a908d7e0dc2623fa402b3239b24a08337c
Emmanuel Oh <emmaneugene@outlook.com>
2024-02-05 09:32:59 +0800

    Add `checkwifi` and split helper scripts between macos and linux

---
+++ dotfiles/linux.helpers.sh
@@ -0,0 +1,47 @@
+# Collection of aliases, helper functions and scripts for UNIX-based systems
+alias fk="fuck"
+alias routerip="curl https://api.incolumitas.com | jq ."
+
+# Converts a video(.mp4) to GIF format
+function vidtogif() {
+  if [ $# -ne 2 ]; then
+    echo "Usage: $0 <inputFile> <outputFile>
+    where input file is of type: .mp4, .webm, ...
+    and output file is .gif"
+  fi
+  gifski -r 10 $1 -o $2
+}
+
+# Invoke manpage or --help for a binary
+function doc() {
+  if [ $# -ne 1 ]; then
+    echo "Usage: $0 <binary>"
+    exit 1
+  fi
+
+  binary="$1"
+
+  if man -w "$binary" >/dev/null 2>&1; then
+    man "$binary"
+  else
+    "$binary" --help
+  fi
+}
+
+# Show basic info on wifi interfaces
+function checkwifi() {
+  interface=$(nmcli -t -f active,device d wifi list | grep '^yes' | cut -d':' -f2)
+  echo -e "\n        \033[1;33mInterface:\033[0m $interface"
+  signal_strength=$(iw dev $interface link | awk '/signal/ {print $2}')
+  echo -e "        \033[1;33mSignal strength:\033[0m $signal_strength"
+  signal_quality=$(nmcli d wifi list ifname $interface | grep '*' | awk '{print $8}')
+  echo -e "        \033[1;33mSignal quality:\033[0m $signal_quality"
+  signal_bars=$(nmcli -t -f active,bars d wifi list | grep '^yes' | awk -F: '{print $2}')
+  echo -e "        \033[1;33mSignal bars:\033[0m $signal_bars"
+  speed=$(nmcli -t -f IN-USE,RATE dev wifi list | grep '*' | awk -F: '{print $2}')
+  echo -e "        \033[1;33mWi-Fi Speed:\033[0m $speed"
+  channel=$(nmcli -t -f IN-USE,CHAN dev wifi list | grep '*' | awk -F: '{print $2}')
+  echo -e "        \033[1;33mWi-Fi Channel:\033[0m $channel"
+  noise=$(awk 'NR==3 {print "Link: " $3 " Level: " $4 " Noise: " $5}' /proc/net/wireless)
+  echo -e "        \033[1;33mWi-Fi Noise, Link and Quality Level:\033[0m $noise";
+}
